fdd246ca6ccec702c1852225ea3d89bdaa26c380
Kazuya MIYAGI <umi.kukki@gmail.com>
2019-04-26 16:21:31 +0900

    Add iam

--- ecs.tf
+++ ecs.tf
@@ -1,9 +1,14 @@
+resource "aws_ecs_cluster" "laravel-cluster" {
+  name = "laravel-cluster"
+}
+
 resource "aws_ecs_task_definition" "laravel" {
-  family       = "laravel"
-  network_mode = "awsvpc"
-  cpu          = "256"
-  memory       = "512"
-  container_definitions = templatefile("task-definitions/laravel.json.tpl", {
+  family             = "laravel"
+  network_mode       = "awsvpc"
+  cpu                = "256"
+  memory             = "512"
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+  container_definitions = templatefile("templates/laravel.json.tmpl", {
     aws_account = var.aws_account,
     aws_region  = var.aws_region,
     secrets     = merge(var.laravel_environments, var.laravel_secure_environments)
---
+++ iam.tf
@@ -0,0 +1,45 @@
+data "aws_iam_policy_document" "allow_ssm_get_parameters" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "ssm:GetParameters"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "ecs_task_execution_role_asssume_role_policy" {
+  statement {
+    actions = [
+      "sts:AssumeRole"
+    ]
+    principals {
+      type = "Service"
+      identifiers = [
+        "ecs-tasks.amazonaws.com"
+      ]
+    }
+  }
+}
+
+resource "aws_iam_policy" "allow_ssm_get_parameters" {
+  name   = "AllowSsmGetParameters"
+  policy = data.aws_iam_policy_document.allow_ssm_get_parameters.json
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_role_asssume_role_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_attached_policy_1" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_attached_policy_2" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = aws_iam_policy.allow_ssm_get_parameters.arn
+}
