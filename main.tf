b69d81901be25fc0fd78b54c1f581f4eb1d1da74
Kazuya MIYAGI <umi.kukki@gmail.com>
2019-06-04 11:47:47 +0900

    Add comment to main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,15 @@
+# https://www.terraform.io/docs/providers/aws/d/caller_identity.html
+# account data
 data "aws_caller_identity" "current" {}
+
+# https://www.terraform.io/docs/providers/aws/d/region.html
+# region data
 data "aws_region" "current" {}
+
+# https://www.terraform.io/docs/providers/aws/d/availability_zones.html
+# availability_zones data
 data "aws_availability_zones" "available" {}
+
+# https://www.terraform.io/docs/providers/aws/d/elb_service_account.html
+# elb service account data
 data "aws_elb_service_account" "main" {}
